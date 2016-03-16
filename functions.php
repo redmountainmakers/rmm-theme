0a2b617a529b6f32a667d48176a7f00f0fd1609b
James Nylen <jnylen@gmail.com>
2016-03-16 03:36:20 -0400

    Style fixes

--- functions.php
+++ functions.php
@@ -5,20 +5,25 @@
 //
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array('parent-style')
-    );
+	wp_enqueue_style(
+		'parent-style',
+		get_template_directory_uri() . '/style.css'
+	);
+	wp_enqueue_style(
+		'child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		array('parent-style')
+	);
 }
 
-if ( function_exists ('register_sidebar') )
-register_sidebar( array(
-  'name' => __( 'Header Widgets Area', 'rmm-sporty' ),
-  'id' => 'sidebar-header',
-  'description' => __( 'Header widgets area for my rmm-sporty child theme.'),
-  'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-  'after_widget' => '</aside>',
-  'before_title' => '<h3 class="widget-title">',
-  'after_title' => '</h3>',
-) );
+if ( function_exists ('register_sidebar') ) {
+	register_sidebar( array(
+		'name'          => __( 'Header Widgets Area', 'rmm-sporty' ),
+		'id'            => 'sidebar-header',
+		'description'   => __( 'Header widgets area for rmm-sporty child theme.'),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
+}
